01e76d9ecd39c1dceed3f8ef24bfd119b4825093
gebeto <slavik.nychkalo@gmail.com>
2019-03-21 15:14:19 +0200

    update svg and canvas drawing

--- src/index.js
+++ src/index.js
@@ -1,193 +1,74 @@
 import ChartsData from '../assets/chart_data.json';
 
-const svgForData = (chartData) => {
-	const calculateHeight = (ys) => {
-		return Math.max.apply(null, ys.map(arr => Math.max.apply(null, arr.slice(1))))
+function normalize(min, max) {
+    const delta = max - min;
+    const memo = {};
+    return (val) => {
+    	if (!memo[val]) {
+    		memo[val] = (val - min) / delta;
+    	}
+    	return memo[val];
+    };
+}
+
+
+const el = document.createElement('canvas');
+document.body.appendChild(el);
+el.width = 1000;
+el.height = 300;
+
+const flatMax = (arr) => Math.max.apply(null, arr.map(set => Math.max.apply(null, set.slice(1))));
+const flatMin = (arr) => Math.min.apply(null, arr.map(set => Math.min.apply(null, set.slice(1))));
+
+
+class Chart {
+	constructor(data, canvas) {
+		this.data = data;
+		const [segments, ...coords] = this.data.columns;
+		this.segments = segments;
+		this.dataSets = coords;
+
+		this.canvas = canvas;
+		this.ctx = canvas.getContext('2d');
+
+		this.segmentsCount = this.segments.length - 1;
+		this.maxHeight = flatMax(this.dataSets);
+		this.minHeight = flatMin(this.dataSets);
+		this.normH = normalize(this.minHeight, this.maxHeight);
+		this.normW = normalize(0, this.segmentsCount - 1);
 	}
 
-	const WIDTH = 1000;
-	// const HEIGHT = 500;
-	const HEIGHT = calculateHeight(chartData.columns.slice(1));
+	drawRow(dataset) {
+		const [name, ...ds] = dataset;
+		const ctx = this.ctx;
+		const h = this.canvas.height;
+		const w = this.canvas.width;
 
-	const createSVG = () => {
-		const svgElement = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-		svgElement.setAttribute('width', `${WIDTH}px`);
-		svgElement.setAttribute('height', `${HEIGHT}px`);
-		svgElement.setAttribute('viewBox', `0 0 ${WIDTH} ${HEIGHT}`);
-		svgElement.setAttribute('preserveAspectRatio', 'none');
-		svgElement.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
-
-		// Rotate
-		const g1 = document.createElementNS('http://www.w3.org/2000/svg', 'g');
-		g1.setAttribute('transform', `translate(0, ${HEIGHT})`);
-
-		const g2 = document.createElementNS('http://www.w3.org/2000/svg', 'g');
-		g2.setAttribute('transform', 'scale(1, -1)');
-
-		g1.appendChild(g2);
-		svgElement.appendChild(g1);
-
-		return {
-			element: svgElement,
-			addPoly: (el) => {
-				g2.appendChild(el)
-			}
-		};
-	}
-
-
-	const createPoly = (data, index, opts) => {
-		const points = [];
-		const [name, ...rest] = data.columns[index];
-		const range = [0, rest.length];
-		const sw = WIDTH / (rest.length - 1);
-		for (let i = 0; i < rest.length; i++) {
-			points.push(`${i * sw},${rest[i]}`);
+		ctx.beginPath();
+		ctx.moveTo(this.normW(0) * w, h - this.normH(ds[0]) * h);
+		for (let i = 1; i < this.segmentsCount; i++) {
+			ctx.lineTo(this.normW(i) * w, h - this.normH(ds[i]) * h );
 		}
-		const poly = document.createElementNS('http://www.w3.org/2000/svg', 'polyline');
-		poly.setAttribute('points', points.join(' '));
-		poly.setAttribute('stroke', data.colors[name]);
-		poly.setAttribute('fill', 'none');
-		poly.setAttribute('stroke-width', opts.sw || '2');
-		poly.setAttribute('vector-effect', 'non-scaling-stroke');
+		ctx.lineWidth = 2;
+		ctx.strokeStyle = this.data.colors[name];
+		ctx.stroke();
+	}
 
-		const g = document.createElementNS('http://www.w3.org/2000/svg', 'g');
-		g.setAttribute('style', 'transition: transform 0.4s, opacity 0.2s;')
-		g.appendChild(poly);
-
-		const updatePoints = () => {
-			const points = [];
-			const sw = WIDTH / (range[1] - range[0] - 1);
-			for (let i = 0; i + range[0] < range[1]; i++) {
-				points.push(`${i * sw},${rest[i + range[0]]}`);
-			}
-			poly.setAttribute('points', points.join(' '));
+	render() {
+		this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+		for (let i = 0; i < this.dataSets.length; i++) {
+			this.drawRow(this.dataSets[i]);
 		}
-
-		return {
-			element: g,
-			hide: () => {
-				g.setAttribute('opacity', '0');
-				g.setAttribute('transform', 'scale(1, 0)');
-			},
-			show: () => {
-				g.setAttribute('opacity', '1');
-				g.setAttribute('transform', 'scale(1, 1)');
-			},
-			clone: () => {
-				return g.cloneNode(true);
-			}
-		};
 	}
-
-	const svg = createSVG();
-	document.body.appendChild(svg.element);
-
-	const poly1 = createPoly(chartData, 1, {});
-	const poly2 = createPoly(chartData, 2, {});
-	svg.addPoly(poly1.element);
-	svg.addPoly(poly2.element);
-
-
-	const preview = createSVG();
-	preview.element.setAttribute('height', '80px')
-	document.body.appendChild(preview.element);
-	const ppoly1 = createPoly(chartData, 1, {sw: 1});
-	const ppoly2 = createPoly(chartData, 2, {sw: 1});
-	preview.addPoly(ppoly1.element);
-	preview.addPoly(ppoly2.element);
-
-
-	const createRect = () => {
-		const rect = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
-		rect.setAttribute('x', '0%');
-		rect.setAttribute('y', '0%');
-		rect.setAttribute('width', '100%');
-		rect.setAttribute('height', '100%');
-		rect.setAttribute('fill', 'red');
-		rect.setAttribute('opacity', '0.5');
-
-		const state = {
-			moving: false,
-			point: 0,
-			offset: 1000,
-		};
-
-		const rectToggle = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
-		rectToggle.setAttribute('x', `1000px`);
-		rectToggle.setAttribute('y', '0%');
-		rectToggle.setAttribute('width', '10px');
-		rectToggle.setAttribute('height', '100%');
-		rectToggle.setAttribute('fill', 'red');
-
-		const g = document.createElementNS('http://www.w3.org/2000/svg', 'g');
-		g.setAttribute('style', `transform: translateX(-${state.offset}px)`);
-		g.appendChild(rect);
-		g.appendChild(rectToggle);
-
-
-		const update = () => {
-			console.log();
-		}
-
-		rectToggle.addEventListener('mousedown', (e) => {
-			console.log(e);
-			state.moving = true;
-		});
-		rectToggle.addEventListener('mouseup', (e) => {
-			console.log(e);
-			state.moving = false;
-		});
-		rectToggle.addEventListener('mousemove', (e) => {
-			if (state.moving) {
-				console.log(e);
-				// console.dir(g.parentNode);
-				const br = g.parentNode.getBoundingClientRect();
-				const offAcc = 1000 - (1000 / g.parentNode.clientWidth * (e.clientX - br.x));
-				state.offset = offAcc;
-				g.setAttribute('style', `transform: translateX(-${state.offset}px)`);
-				console.log(offAcc);
-			}
-		});
-
-		return g;
-	}
-	const rect1 = createRect();
-	preview.element.appendChild(rect1);
+}
 
 
 
-	const values = {
-		scale: 1000,
-		x: 0,
-	};
-	const changeView = () => {
-		console.log(values.scale)
-		const x = values.x;
-		const y = 0;
-		// const x = 1000 - values.scale - values.x;
-		// const y = (1000 - values.scale) / 2;
-		const width = values.scale;
-		// const height = values.scale / 2;
-		const height = HEIGHT;
-		svg.element.setAttribute('viewBox', `${x} ${y} ${width} ${height}`)
-	}
+const c = ChartsData[0];
+const chart = new Chart(c, el);
+chart.render();
 
-
-	document.getElementById('range-scale').addEventListener('input', (e) => {
-		console.log(e.target.value);
-		values.scale = parseInt(e.target.value);
-		changeView();
-		document.getElementById('range-x').setAttribute('min', values.scale)
-	});
-
-	document.getElementById('range-x').addEventListener('input', (e) => {
-		console.log(e.target.value);
-		values.x = 1000 - parseInt(e.target.value);
-		changeView();
-	});
-};
-
-
-
-svgForData(ChartsData[0])
+// (function loop() {
+// 	chart.render();
+// 	requestAnimationFrame(loop);
+// })();
---
+++ src/index1.js
@@ -0,0 +1,203 @@
+import ChartsData from '../assets/chart_data.json';
+
+function normalize(min, max) {
+    var delta = max - min;
+    return function (val) {
+        return (val - min) / delta;
+    };
+}
+
+const svgForData = (chartData) => {
+	const calculateHeight = (ys) => {
+		return Math.max.apply(null, ys.map(arr => Math.max.apply(null, arr.slice(1))))
+	}
+
+	const WIDTH = 1000;
+	// const HEIGHT = 500;
+	const HEIGHT = calculateHeight(chartData.columns.slice(1));
+
+	const createSVG = () => {
+		const svgElement = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+		svgElement.setAttribute('width', `${WIDTH}px`);
+		svgElement.setAttribute('height', `${HEIGHT}px`);
+		svgElement.setAttribute('viewBox', `0 0 ${WIDTH} ${HEIGHT}`);
+		svgElement.setAttribute('preserveAspectRatio', 'none');
+		svgElement.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
+
+		// Rotate
+		const g1 = document.createElementNS('http://www.w3.org/2000/svg', 'g');
+		g1.setAttribute('transform', `translate(0, ${HEIGHT})`);
+
+		const g2 = document.createElementNS('http://www.w3.org/2000/svg', 'g');
+		g2.setAttribute('transform', 'scale(1, -1)');
+
+		g1.appendChild(g2);
+		svgElement.appendChild(g1);
+
+		return {
+			element: svgElement,
+			addPoly: (el) => {
+				g2.appendChild(el)
+			}
+		};
+	}
+
+
+	const createPoly = (data, index, opts) => {
+		const points = [];
+		const [name, ...rest] = data.columns[index];
+		const range = [0, rest.length];
+		const sw = WIDTH / (rest.length - 1);
+		for (let i = 0; i < rest.length; i++) {
+			points.push(`${i * sw},${rest[i]}`);
+		}
+		const poly = document.createElementNS('http://www.w3.org/2000/svg', 'polyline');
+		poly.setAttribute('points', points.join(' '));
+		poly.setAttribute('stroke', data.colors[name]);
+		poly.setAttribute('fill', 'none');
+		poly.setAttribute('stroke-width', opts.sw || '2');
+		poly.setAttribute('vector-effect', 'non-scaling-stroke');
+
+		const g = document.createElementNS('http://www.w3.org/2000/svg', 'g');
+		g.setAttribute('style', 'transition: transform 0.4s, opacity 0.2s;')
+		g.appendChild(poly);
+
+		const updatePoints = () => {
+			const points = [];
+			const sw = WIDTH / (range[1] - range[0] - 1);
+			for (let i = 0; i + range[0] < range[1]; i++) {
+				points.push(`${i * sw},${rest[i + range[0]]}`);
+			}
+			poly.setAttribute('points', points.join(' '));
+		}
+
+		return {
+			element: g,
+			hide: () => {
+				g.setAttribute('opacity', '0');
+				g.setAttribute('transform', 'scale(1, 0)');
+			},
+			show: () => {
+				g.setAttribute('opacity', '1');
+				g.setAttribute('transform', 'scale(1, 1)');
+			},
+			clone: () => {
+				return g.cloneNode(true);
+			}
+		};
+	}
+
+	const svg = createSVG();
+	document.body.appendChild(svg.element);
+
+	const poly1 = createPoly(chartData, 1, {});
+	const poly2 = createPoly(chartData, 2, {});
+	// const poly3 = createPoly(chartData, 3, {});
+	svg.addPoly(poly1.element);
+	svg.addPoly(poly2.element);
+	// svg.addPoly(poly3.element);
+
+
+	const preview = createSVG();
+	preview.element.setAttribute('height', '80px')
+	document.body.appendChild(preview.element);
+	const ppoly1 = createPoly(chartData, 1, {sw: 1});
+	const ppoly2 = createPoly(chartData, 2, {sw: 1});
+	preview.addPoly(ppoly1.element);
+	preview.addPoly(ppoly2.element);
+
+
+	const createRect = (svg) => {
+		const rect = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
+		rect.setAttribute('x', '0%');
+		rect.setAttribute('y', '0%');
+		rect.setAttribute('width', '100%');
+		rect.setAttribute('height', '100%');
+		rect.setAttribute('fill', 'red');
+		rect.setAttribute('opacity', '0.5');
+
+		const state = {
+			moving: false,
+			point: 0,
+			offset: 1000,
+		};
+
+		const rectToggle = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
+		rectToggle.setAttribute('x', `1000px`);
+		rectToggle.setAttribute('y', '0%');
+		rectToggle.setAttribute('width', '10px');
+		rectToggle.setAttribute('height', '100%');
+		rectToggle.setAttribute('fill', 'red');
+
+		const g = document.createElementNS('http://www.w3.org/2000/svg', 'g');
+		g.setAttribute('style', `transform: translateX(-${state.offset}px)`);
+		g.appendChild(rect);
+		g.appendChild(rectToggle);
+
+
+		const update = () => {
+			console.log();
+		}
+
+		svg.addEventListener('mousedown', (e) => {
+			console.log(e);
+			state.moving = true;
+		});
+		svg.addEventListener('mouseup', (e) => {
+			console.log(e);
+			state.moving = false;
+		});
+		svg.addEventListener('mousemove', (e) => {
+			console.log('MOVE')
+			if (state.moving) {
+				console.log(e);
+				// console.dir(g.parentNode);
+				const br = g.parentNode.getBoundingClientRect();
+				const offAcc = 1000 - (1000 / g.parentNode.clientWidth * (e.clientX - br.x));
+				state.offset = offAcc;
+				g.setAttribute('style', `transform: translateX(-${state.offset}px)`);
+				console.log(offAcc);
+			}
+		});
+
+		return g;
+	}
+	const rect1 = createRect(preview.element);
+	preview.element.appendChild(rect1);
+
+
+
+	const values = {
+		scale: 1000,
+		x: 0,
+	};
+	const changeView = () => {
+		console.log(values.scale)
+		const x = values.x;
+		const y = 0;
+		// const x = 1000 - values.scale - values.x;
+		// const y = (1000 - values.scale) / 2;
+		const width = values.scale;
+		// const height = values.scale / 2;
+		const height = HEIGHT;
+		svg.element.setAttribute('viewBox', `${x} ${y} ${width} ${height}`)
+	}
+
+
+	document.getElementById('range-scale').addEventListener('input', (e) => {
+		console.log(e.target.value);
+		values.scale = parseInt(e.target.value);
+		changeView();
+		document.getElementById('range-x').setAttribute('min', values.scale)
+	});
+
+	document.getElementById('range-x').addEventListener('input', (e) => {
+		console.log(e.target.value);
+		values.x = 1000 - parseInt(e.target.value);
+		changeView();
+	});
+};
+
+
+
+svgForData(ChartsData[0])
