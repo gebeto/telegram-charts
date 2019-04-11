0731497dbac497fdf9b1621d8d2fdc7f2d539623
gebeto <slavik.nychkalo@gmail.com>
2019-04-11 10:57:21 +0300

    try to add chart optimization

--- src/Drawers/LineChart.js
+++ src/Drawers/LineChart.js
@@ -19,11 +19,23 @@ export default function LineChartDrawer(drawersArgs) {
 	const drawLineLayer  = drawingWithRange(control.range, LineRangeLayerDrawer(drawersArgs));
 	const drawDotsLayer  = drawingWithRange(control.range, DotsLayerDrawer(drawersArgs));
 
+	// const backCanvas = document.createElement('canvas');
+	// const backCtx = backCanvas.getContext('2d');
+
+	// backCanvas.style.border = '1px solid red';
+	// document.body.appendChild(backCanvas);
+
+	const lastRange = [control.range[0], control.range[1], control.range[1] - control.range[0]];
+
+
 	return function drawChart(x, y, width, height) {
 		// ctx.save();
 		// ctx.fillStyle = 'green';
 		// ctx.fillRect(x, y, width, height);
 		// ctx.restore();
+
+		// backCtx.fillRect(0, 0, 100, 100);
+
 
 		const HEIGHT = height - BOTTOM_PADDING
 
