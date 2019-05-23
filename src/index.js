8ae1bc5c44a37dc6343e7097e61b8b463f3f4ffc
gebeto <slavik.nychkalo@gmail.com>
2019-05-23 15:53:23 +0300

    add auto fabrics selection by dataset, refactor styles

--- src/index.js
+++ src/index.js
@@ -25,44 +25,53 @@ createThemeChanger((isLight) => {
 });
 
 
-const fabrics = [
-	{
-		drawChartFabric: (args) => LineChartDrawer(args),
-		drawControlFabric: (args) => LineControlsDrawer(args),
-	},
-	{
-		drawChartFabric: (args) => DualLineChartDrawer(args),
-		drawControlFabric: (args) => LineControlsDrawer(args),
-	},
-	{
-		drawChartFabric: (args) => BarChartDrawer(args),
-		drawControlFabric: (args) => BarControlsDrawer(args),
-	},
-	{
-		drawChartFabric: (args) => BarChartDrawer(args),
-		drawControlFabric: (args) => BarControlsDrawer(args),
-	},
-	{
-		drawChartFabric: (args) => FillLineChartDrawer(args),
-		drawControlFabric: (args) => FillLineControlsDrawer(args),
-	},
-	{
-		drawChartFabric: (args) => AreaChartDrawer(args),
-		drawControlFabric: (args) => FillLineControlsDrawer(args),
-	},
-];
+function fabricByDatasource(datasource) {
+	const fabric = {
+		drawChartFabric: null,
+		drawControlFabric: null,
+	};
+	// const [xaxis, ...yaxis] = datasource.columns;
+	// const axis = yaxis.map(axi => axi[0]);
+	// console.log(axis, datasource.types);
+	const types = Object.keys(datasource.types).map(key => datasource.types[key]).filter(el => el !== 'x');
+	if (!types.length) {
+		throw new Error("No data types for chart")
+	} else if (types.length > 50) {
+		throw new Error("Too much data. Supported up to 50 columns on one graph.")
+	}
+
+	const type = types[0];
+	console.log(type);
+	if (datasource.y_scaled) {
+		fabric.drawChartFabric = (args) => DualLineChartDrawer(args);
+		fabric.drawControlFabric = (args) => LineControlsDrawer(args);
+	} else if (type == 'bar') {
+		fabric.drawChartFabric = (args) => BarChartDrawer(args);
+		fabric.drawControlFabric = (args) => BarControlsDrawer(args);
+	} else if (type == 'area') {
+		fabric.drawChartFabric = (args) => FillLineChartDrawer(args);
+		fabric.drawControlFabric = (args) => FillLineControlsDrawer(args);
+		fabric.drawZoomedChartFabric = (args) => AreaChartDrawer(args);
+	} else {
+		fabric.drawChartFabric = (args) => LineChartDrawer(args);
+		fabric.drawControlFabric = (args) => LineControlsDrawer(args);
+	}
+
+	return fabric;
+}
 
 
 function initGraphModule() {
 	let index = 0;
 
 	return {
-		render: function Graph(container, chartData, fabric, opts = {}) {
+		render: function Graph(container, chartData, opts = {}) {
 			const graphOpts = {
 				container: container || document.body,
 				index: index++,
 				title: opts.title || undefined
 			};
+			const fabric = fabricByDatasource(chartData);
 			const graph = Chart(graphOpts, chartData, fabric);
 			AnimationLoop.add(() => {
 				graph.render();
@@ -74,5 +83,4 @@ function initGraphModule() {
 }
 
 
-window.GraphFabrics = fabrics;
 window.Graph = initGraphModule();
