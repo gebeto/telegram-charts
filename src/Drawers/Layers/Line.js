22049098662f0208246aa184b3bc9e5398071af6
gebeto <slavik.nychkalo@gmail.com>
2019-05-23 17:34:44 +0300

    add some optimizations for line chart layer

--- src/Drawers/Layers/Line.js
+++ src/Drawers/Layers/Line.js
@@ -17,13 +17,20 @@ export default function Line({ config, control, ctx, norm, colors, normYKey }, o
 		const count = items.length;
 		ctx.save();
 		ctx.beginPath();
+		const chunkSize = norm.X(1) * width;
 		const yh = y + height;
 		ctx.moveTo(x + 0, yh - normY(items[0]) * height);
+		// const offset = Math.abs(x / chunkSize) >> 0;
+		// console.log('XXX', x, chunkSize, offset);
 		for (let i = 1; i < count; i++) {
-			const X = x + norm.X(i) * width;
+			// const X = x + norm.X(i) * width;
+			const X = x + chunkSize * i;
 			const Y = yh - normY(items[i]) * height;
 			if (Y > yh) {
 				config.shouldControlUpdate = true;
+			}
+			if (X < -chunkSize || X > ctx.canvas.width + chunkSize) {
+				continue;
 			}
 			ctx.lineTo(X, Y);
 		}
